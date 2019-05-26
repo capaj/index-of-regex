29bc22f5d782eacc8217ac77dec64d1dcc9327a4
Zhu Zijia <littlepiggy03@hotmail.com>
2019-05-26 10:35:17 +0800

    add type definition file

---
+++ types/index.d.ts
@@ -0,0 +1,2 @@
+export function indexOfRegex(str: string, regex: RegExp, startPosition?: number): number;
+export function lastIndexOfRegex(str: string, regex: RegExp, startPosition?: number): number;
